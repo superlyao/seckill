dac9740386eab93bf173ab1e904a8202cc90e5ff
yliao <superyao12@sina.com>
2018-04-03 22:30:33 +0800

    日志工具类

---
+++ src/main/java/com/yliao/seckill/log/LoggerUtil.java
@@ -0,0 +1,12 @@
+package com.yliao.seckill.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author: yliao
+ * @Date: Created in 22:33 2018/4/1
+ */
+public class LoggerUtil {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+}
